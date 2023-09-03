9ad65841e36563886c637dc249d61c2b2b43cfde
kiz0r <kkhomenko10@gmail.com>
2023-09-03 13:33:30 +0200

    Fix Logo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -119,11 +119,8 @@ class Header extends React.Component {
           </div>
         </div>
         <div className={styles.navContainer}>
-          <img
-            src={`${CONSTANTS.STATIC_IMAGES_PATH}blue-logo.png`}
-            className={styles.logo}
-            alt='blue_logo'
-          />
+          <Logo alt='blue_logo' className={styles.logo} />
+
           <div className={styles.leftNav}>
             <div className={styles.nav}>
               <ul>
